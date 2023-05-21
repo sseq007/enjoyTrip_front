f7f85a6c5ef20dd1958bf214b162dc0effa3b376
김주하 <dnsfudcjsfud@gmail.com>
2023-05-21 21:29:00 +0900

    회원 등록 연동

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -78,8 +78,7 @@
 								</a>
 								<ul class="dropdown-menu">
 									<li><router-link :to="'/user/userLogin'" class="dropdown-item">로그인</router-link></li>
-									<li><a class="dropdown-item" href="#" data-bs-toggle="modal"
-											data-bs-target="#signUpModal">회원가입</a>
+									<li><router-link :to="'/user/userRegister'" class="dropdown-item">회원가입</router-link>
 									</li>
 								</ul>
 							</li>
@@ -107,89 +106,11 @@
 
 		<!-- [E] navbar -->
 		<div style="height: 77px"></div>
-
-		<!-- [S] sign up modal-->
-		<div class="modal fade" id="signUpModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-			aria-labelledby="staticBackdropLabel" aria-hidden="true">
-			<div class="modal-dialog modal-lg">
-				<div class="modal-content">
-					<!-- Modal Header -->
-					<div class="modal-header">
-						<h4 class="modal-title">
-							<i class="fi fi-rr-lock ps-3 fs">Sign Up</i>
-						</h4>
-						<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-					</div>
-
-					<!-- Modal Body -->
-					<div class="modal-body">
-						<form action="" method="post" id="join-form" encType="multipart/form-data">
-							<!-- <input type="hidden" name="action" value="join" /> -->
-							<div class="mb-3 mt-3" align="center">
-								<div class="selected">
-									<img src="@/assets/img/noimg.jpg" class="profile_image"
-										style="border-radius: 40%; width: 350px; height: 350px">
-								</div>
-								<div>
-									<input type="file" id="profile" name="file" accept=".jpg, .png, .jpeg" value="" />
-								</div>
-
-
-							</div>
-							<div class="mb-3 mt-3">
-								<label for="name" class="form-label">이름:</label> <input type="text" class="form-control"
-									id="name" placeholder="Enter name" name="userName" ref="userName" />
-							</div>
-							<div class="mb-3">
-								<label for="id" class="form-label">ID:</label> <input type="text" class="form-control"
-									id="id" placeholder="Enter ID" name="userId" ref="userId" />
-							</div>
-							<div id="idcheck-result"></div>
-							<div class="mb-3">
-								<label for="pw" class="form-label">Password:</label> <input type="password"
-									class="form-control" id="pw" placeholder="Enter password" name="userPw" ref="userPw" />
-							</div>
-							<div class="mb-3">
-								<label for="pwCheck" class="form-label">Password check:</label> <input type="password"
-									class="form-control" id="pwCheck" placeholder="비밀번호 확인" name="regipw-check"
-									ref="pwCheck" />
-							</div>
-							<div class="form-row align-items-center">
-								<div class="col-sm-10 my-1">
-									<label class="form-label" for="emailId">Email:</label>
-									<div class="input-group">
-										<input type="text" class="form-control" id="emailId" ref="emailId" name="emailId"
-											placeholder="이메일 아이디" />
-										<div class="input-group-prepend">
-											<div class="input-group-text">@</div>
-										</div>
-										<select id="emailDomain" name="emailDomain" ref="emailDomain" class="form-control">
-											<option selected>도메인 선택</option>
-											<option value="naver.com">naver.com</option>
-											<option value="gmail.com">gmail.com</option>
-											<option value="daum.net">daum.net</option>
-										</select>
-									</div>
-								</div>
-							</div>
-						</form>
-					</div>
-
-					<!-- Modal Footer -->
-					<div class="modal-footer">
-						<button type="button" id="btn-mv-join" class="btn btn-outline-secondary"
-							@click="checkValue">회원가입</button>
-					</div>
-				</div>
-			</div>
-		</div>
-
 	</div>
 </template>
 
 <script>
 import { mapState, mapGetters, mapActions } from "vuex";
-import { registerMember } from "@/api/member";
 import noteListItem from '../components/note/noteListItem.vue';
 import axios from "axios";
 const memberStore = "memberStore";
@@ -242,48 +163,6 @@ export default {
 			this.modalVisible = false;
 		},
 		...mapActions(memberStore, ["userConfirm", "getUserInfo", "userLogout"]),
-		checkValue() {
-			let err = true;
-			let msg = "";
-
-			err && !this.$refs.userName.value && ((msg = "이름을 입력해 주세요"), (err = false), this.$refs.userName.focus());
-			err && !this.$refs.userId.value && ((msg = "id를 입력해주세요"), (err = false), this.$refs.userId.focus());
-			err && !this.$refs.userPw.value && ((msg = "비밀번호를 입력해 주세요"), (err = false), this.$refs.userPw.focus());
-
-
-			if (this.$refs.userPw.value != this.$refs.pwCheck.value) {
-				err = false;
-				msg = "비밀번호를 다시 확인해 주세요";
-			}
-			if (!err) alert(msg);
-			// 만약, 내용이 다 입력되어 있다면 registArticle 호출
-			else this.register();
-		},
-		async register() {
-			var formdata = {
-				userName: this.$refs.userName.value,
-				userId: this.$refs.userId.value,
-				userPw: this.$refs.userPw.value,
-				emailDomain: this.$refs.emailDomain.value,
-				emailId: this.$refs.emailId.value
-			}
-			console.log(formdata);
-			registerMember(
-				formdata,
-				({ data }) => {
-					let msg = "멤버 등록을 실패했습니다.";
-					if (data.message === "success") {
-						msg = "등록이 완료되었습니다.";
-					}
-					alert(msg);
-
-					location.href = "/";
-				},
-				(error) => {
-					console.log(error);
-				}
-			);
-		},
 		onClickLogout() {
 			console.log(this.userInfo.userId);
 			this.userLogout(this.userInfo.userId);
--- src/components/user/userRegister.vue
+++ src/components/user/userRegister.vue
@@ -3,25 +3,133 @@
         <div style="background-image:url('../assets/img/registerDiv.png');
         background-repeat: no-repeat;
         background-size: contain; position : relative;
-        height: 1400px; margin-top: 70px; margin-left: 10%;  display: flex; justify-content: center;">
-            <img :src="image" class="profile_image" width="300px" height="300px" style="display: block; margin-right: 130px; margin-top: 17px; border-radius: 50%; position: absolute;">
-            <div style="margin-left: 3%; padding-top: 1px">
-                <h2 style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 100px; font-weight: 600; " class="m-0 mt-5">LOGIN</h2>
-                <hr class="mb-3 mt-0" align="left" style="border: solid 5px #85c6d7; width: 20%" />
-            </div>
-            <div style="vertical-align: middle;">
-                <label className="input-file-button" for="updateProfile" style="margin-right: 130px; margin-top: 100px; height: 60px; width: 160px; background-color: transparent; border-radius: 20px; opacity: 60%;
-                border:4px solid #85c6d7; float: right; text-align: center
-                font-family: 'Nixgon, sans-serif; color: #85c6d7; font-weight:600; font-size: 30px;">
-                UPLOAD
-                </label>
-                <input ref="image" type="file" id="updateProfile" name="file" accept=".jpg, .png, .jpeg" value="" style="display: none" @change="imageChange()"/>
+        height: 1700px; margin-top: 70px; margin-left: 10%;  display: flex; justify-content: center;">
+            <img :src="image" class="profile_image" width="310px" height="310px" style="display: block; margin-right: 103px; margin-top: 16px; border-radius: 50%; position: absolute;">
+            <div style="flex-direction: column;">
+                <div style="margin-right: 200px; margin-top: 120px; padding-top: 1px; display: flex; height: 200px;">
+                    <h2 style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 100px; font-weight: 600; " class="m-0 mt-5">SIGN</h2>
+                    <div style="vertical-align: middle;">
+                        <label className="input-file-button" for="updateProfile" style="margin-left: 280px; margin-right: 30px; margin-top: 110px; height: 80px; width: 80px; background-color: #FFE4E0; border-radius: 40%; opacity: 60%;
+                        border:4px solid #85c6d7; float: right; text-align: center; padding-top: 2px;
+                        font-family: 'Nixgon, sans-serif; color: #85c6d7; font-weight:600; font-size: 30px;">
+                            <i class="fi fi-rr-camera" style="font-size: 50px; color: #85c6d7;"></i>
+                        </label>
+                        <input ref="image" type="file" id="updateProfile" name="file" accept=".jpg, .png, .jpeg" value="" style="display: none" @change="imageChange()"/>
+                    </div>
+                    <h2 style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 100px; font-weight: 600;" class="m-0 mt-5">UP</h2>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 50px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 아이디
+                    </label>
+                    <input type="text" v-model="userId" style="margin-top: 8px; font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                    width:760px; height:55px; padding-left: 15px; background-color: transparent;
+                    border:4px solid #85c6d7; float:left; font-size: 28px" placeholder="ID"
+                    required/>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 15px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 비밀번호
+                    </label>
+                    <input type="password" v-model="userPw" style="margin-top: 8px; font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                    width:760px; height:55px; padding-left: 15px; background-color: transparent;
+                    border:4px solid #85c6d7; float:left; font-size: 28px" placeholder="Password"
+                    required/>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 15px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 비밀번호 재확인
+                    </label>
+                    <input type="password" v-model="pwCheck" style="margin-top: 8px; font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                    width:760px; height:55px; padding-left: 15px; background-color: transparent;
+                    border:4px solid #85c6d7; float:left; font-size: 28px" placeholder="Password Check"
+                    required/>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 15px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 이름
+                    </label>
+                    <input type="text" v-model="userName" style="margin-top: 10px; font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                    width:760px; height:55px; padding-left: 15px; background-color: transparent;
+                    border:4px solid #85c6d7; float:left; font-size: 28px" placeholder="Name"
+                    required/>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 15px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 닉네임
+                    </label>
+                    <input type="text" v-model="userNickname" style="margin-top: 8px; font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                    width:760px; height:55px; padding-left: 15px; background-color: transparent;
+                    border:4px solid #85c6d7; float:left; font-size: 28px" placeholder="Nickname"
+                    required/>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 15px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 성별
+                    </label>
+                    <select ref="gender" style="margin-top: 8px; font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                    width:760px; height:55px; padding-left: 15px; background-color: transparent; appearance: none;
+                    border:4px solid #85c6d7; float:left; font-size: 28px; outline: 0 none; -moz-appearance: none;
+                    -webkit-appearance: none;"
+                    required>
+                        <option disabled selected style="display: none;">성별 선택</option>
+                        <option value="male" style="background-color: #FFE4E0;
+                        font-family: 'Nixgon, sans-serif; font-weight:600; color: #939393;">남자</option>
+                        <option value="female" style="background-color: #FFE4E0;
+                        font-family: 'Nixgon, sans-serif; font-weight:600; color: #939393;">여자</option>
+                    </select>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 15px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 생년월일
+                    </label>
+                    <input type="date" style="margin-top: 8px; font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                    width:760px; height:55px; padding-left: 15px; padding-right: 15px; background-color: transparent;
+                    border:4px solid #85c6d7; float:left; font-size: 28px" placeholder="Nickname"
+                    required/>
+                </div>
+                <div style="display: flex; flex-direction: column; margin-left: 3%; margin-top: 15px; height: 95px;">
+                    <label style="font-family: 'Nixgon, sans-serif; opacity: 60%; font-size: 20px; font-weight: 600;">
+                        * 이메일
+                    </label>
+                    <div style="display: flex; margin-top: 8px;">
+                        <input type="text" style="font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                        width:250px; height:55px; padding-left: 15px; background-color: transparent;
+                        border:4px solid #85c6d7; float:left; font-size: 28px" placeholder="example"
+                        required/>
+                        <div style="width:55px; height:55px; opacity: 60%; background-color: #85c6d7;">
+                            <label style="font-family: 'Nixgon, sans-serif; font-weight:600; font-size: 35px; color: #FFE4E0; padding-left: 10px; padding-bottom: 10px;">@</label>
+                        </div>
+                        <select ref="emailDomain" style="font-family: 'Nixgon, sans-serif; font-weight:600; opacity: 60%; 
+                        width:300px; height:55px; padding-left: 15px; background-color: transparent; appearance: none;
+                        border:4px solid #85c6d7; float:left; font-size: 28px; outline: 0 none; -moz-appearance: none;
+                        -webkit-appearance: none;"
+                        required>
+                            <option disabled selected style="display: none;">도메인 선택</option>
+							<option value="naver.com">naver.com</option>
+							<option value="gmail.com">gmail.com</option>
+							<option value="daum.net">daum.net</option>
+                        </select>
+                        <button class="button" style="margin-left: 10px; height: 60px; width: 145px; background-color: transparent; border-radius: 20px; 
+                        opacity: 60%; border:4px solid #85c6d7;">
+                            <span style="font-family: 'Nixgon, sans-serif; color: #85c6d7; font-weight:600; font-size: 30px;">인증</span>
+                        </button>
+                    </div>
+                </div>
+                <div style="display: flex; position: absolute; margin-top: 20px; left: 570px; bottom: 300px; margin-right: 18%; float: right;">
+                    <button class="button" style="height: 60px; width: 320px; background-color: transparent; border-radius: 20px; 
+                    opacity: 60%; border:4px solid #85c6d7;" @click="checkValue">
+                        <span style="font-family: 'Nixgon, sans-serif; color: #85c6d7; font-weight:600; font-size: 30px;">가입하기</span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { registerMember } from "@/api/member";
+
 export default {
     name: "userRegister",
     components:{
@@ -29,6 +137,13 @@ export default {
     },
     data(){
         return{
+            userId: '',
+            userPw: '',
+            pwCheck: '',
+            userName: '',
+            userNickname: '',
+            birth: '',
+            emailId: '',
             image: '',
         }
     },
@@ -40,8 +155,51 @@ export default {
             var image = this.$refs['image'].files[0];
             const url = URL.createObjectURL(image);
             this.image = url;
-            console.log(url);           
+            console.log(url);          
         },
+        checkValue() {
+			let err = true;
+			let msg = "";
+
+			if(err && !this.userId){
+                msg = "ID를 입력해주세요"
+                err = false
+            }
+
+			if (err && this.userPw != this.pwCheck) {
+				err = false;
+				msg = "비밀번호를 다시 확인해 주세요";
+			}
+			if (!err) alert(msg);
+			// 만약, 내용이 다 입력되어 있다면 registArticle 호출
+			else this.register();
+		},
+        async register() {
+			const formdata = new FormData();
+            formdata.append('userId', this.userId);
+            formdata.append('userName', this.userName);
+            formdata.append('userPw', this.userPw);
+            formdata.append('gender', this.$refs.gender.value);
+            formdata.append('emailId', this.emailId);
+            formdata.append('emailDomain', this.$refs.emailDomain.value);
+            formdata.append('file', this.$refs.image.files[0]);
+			console.log(formdata);
+			registerMember(
+				formdata,
+				({ data }) => {
+					let msg = "멤버 등록을 실패했습니다.";
+					if (data.message === "success") {
+						msg = "등록이 완료되었습니다.";
+					}
+					alert(msg);
+
+					this.$router.push({ name: "home"});
+				},
+				(error) => {
+					console.log(error);
+				}
+			);
+		},
     }
 }
 </script>
