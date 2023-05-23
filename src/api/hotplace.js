4df5171e8051045127643b53a9326e77af18301e
김주하 <dnsfudcjsfud@gmail.com>
2023-05-23 15:07:41 +0900

    하트 하는 중

--- src/api/hotplace.js
+++ src/api/hotplace.js
@@ -31,4 +31,17 @@ function deleteHotplace(articleNo, success, fail) {
     api.delete(`/api/hotplace/delete/${articleNo}`).then(success).catch(fail);
 }
 
-export { writeHotplace, modifyHotplace, getHotplace, listHotplace, deleteHotplace };
+function checkHeart(heartDto, success, fail){
+    api.get(`/api/hotplace/like`, JSON.stringify(heartDto)).then(success).catch(fail);
+}
+
+function plusHeart(heartDto, success, fail){
+    api.post(`/api/hotplace/like`, JSON.stringify(heartDto)).then(success).catch(fail);
+}
+
+function minusHeart(heartDto, success, fail){
+    api.delete(`/api/hotplace/like`, JSON.stringify(heartDto)).then(success).catch(fail);
+}
+
+
+export { writeHotplace, modifyHotplace, getHotplace, listHotplace, deleteHotplace, checkHeart, plusHeart, minusHeart };
